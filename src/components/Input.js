b0d967cdc3ae31cdf4459d3d18ae456d570b35b0
David Francisco <davidfco.pozo@hotmail.com>
2022-08-04 08:47:05 -0400

    Fixing regex

--- src/components/Input.js
+++ src/components/Input.js
@@ -9,8 +9,8 @@ const Input = ({ setInputValue }) => {
   const onChangeValidator = (e) => {
     setValue(e.target.value);
     const reg =
-      /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\.{2,6}_~:/?#[\]@!\$&'\(\)\*\+,;=.]{2,6}$/;
-    // => /(?http(s)?:\/\/.)?(\w\.-)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g;
+      /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.{2,6}]+$/;
+
     if (e.target.value.length > 1 && !reg.test(value)) {
       linkInput.style.outlineStyle = "solid";
       linkInput.style.outlineColor = "red";
